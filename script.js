752916acfa0827ed70f30d4d730692c67448d47e
Tara <swingholm79@gmail.com>
2023-02-05 01:53:58 -0600

    add analog clock and date

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
-const dateEl = document.querySelector('.date');
+let dateEl = document.querySelector('.date');
 const toggle = document.querySelector('.toggle');
 
 const days = [
@@ -46,10 +46,12 @@ function setTime() {
   const time = new Date();
   const month = time.getMonth();
   const day = time.getDay();
+  const date = time.getDate();
   const hours = time.getHours();
   const hoursForClock = hours % 12;
   const minutes = time.getMinutes();
   const seconds = time.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
 
   hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
     hoursForClock,
@@ -72,6 +74,11 @@ function setTime() {
     0,
     360
   )}deg)`;
+
+  timeEl.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]}<span class="circle">${date}</span>`;
 }
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
